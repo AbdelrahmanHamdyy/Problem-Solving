96084b8d2f46aaa0405a28de67669177a9e042bb
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-08-03 03:47:14 +0200

    3/8

---
+++ CodeForces/Array Stabilization.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define cin(v) for (int i = 0; i < sz(v); i++) cin >> v[i];
+#define cout(v) for (auto i : v) cout << i << " ";
+constexpr double pi = 3.14159265358979323846;
+
+typedef long long ll;
+typedef unsigned long long usll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef set<int> si;
+
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a * b / gcd(a, b);
+}
+ll fact(ll n) {
+	if (n == 0)
+		return 1;
+	for (ll i = n - 1; i > 1; i--)
+		n *= i;
+	return n;
+}
+
+void solve() {
+	int n; cin >> n;
+	if (n == 2) {
+		cout << 0; 
+		return;
+	}
+	vi v(n); cin(v);
+	sort(all(v));
+	if (v[n - 2] - v[0] < v[n - 1] - v[1])
+		cout << v[n - 2] - v[0];
+	else
+		cout << v[n - 1] - v[1];
+}
+
+int main() {
+	fast;
+	// int t; cin >> t;
+	// while (t--)
+		solve();
+	return 0;
+}
---
+++ CodeForces/The World is a Theatre.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define cin(v) for (int i = 0; i < sz(v); i++) cin >> v[i];
+#define cout(v) for (auto i : v) cout << i << " ";
+constexpr double pi = 3.14159265358979323846;
+
+typedef long long ll;
+typedef unsigned long long usll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef set<int> si;
+
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a * b / gcd(a, b);
+}
+ll fact(ll n) {
+	return n == 1 ? 1 : n * fact(n - 1);
+}
+
+ll nCr(int n, int r)
+{
+	ll g, u = 1, d = 1;
+	for (int i = 0; i < r; ++i)
+	{
+		u = u * (n - i);
+		d = d * (r - i);
+		g = gcd(u, d);
+		u /= g;
+		d /= g;
+	}
+	return u;
+}
+
+void solve() {
+	ll n, m, t; cin >> n >> m >> t;
+	ll numberOfWays = 0;
+	ll i = 4;
+	while (i < t) {
+		numberOfWays += nCr(n, i) * nCr(m, t - i);
+		i++;
+	}
+	cout << numberOfWays;
+}
+
+int main() {
+	fast;
+	// int t; cin >> t;
+	// while (t--)
+		solve();
+	return 0;
+}

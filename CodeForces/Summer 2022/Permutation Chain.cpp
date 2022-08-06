ff672c80ffa53d341941e46d5f823be72bdd8483
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-08-06 23:06:08 +0200

    6/8

---
+++ CodeForces/Summer 2022/Permutation Chain.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define cin(v) for (int i = 0; i < sz(v); i++) cin >> v[i];
+#define cout(v) for (auto i : v) cout << i << " ";
+constexpr double pi = 3.14159265358979323846;
+
+typedef long long ll;
+typedef unsigned long long usll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef set<int> si;
+
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+ll lcm(ll a, ll b) {
+	return a * b / gcd(a, b);
+}
+ll fact(ll n) {
+	return n == 1 ? 1 : n * fact(n - 1);
+}
+
+ll nCr(ll n, ll r)
+{
+	ll g, u = 1, d = 1;
+	for (ll i = 0; i < r; i++) {
+		u = u * (n - i);
+		d = d * (r - i);
+		g = gcd(u, d);
+		u /= g;
+		d /= g;
+	}
+	return u;
+}
+
+bool isPrime(int n) {
+	for (int i = 2; i < n; i++)
+		if (n % i == 0) return false;
+	return true;
+}
+
+void print(vi& v) {
+	int len = sz(v);
+	for (int i = 0; i < len; i++) {
+		if (i < len - 1) cout << v[i] << " ";
+		else cout << v[i];
+	}
+	cout << endl;
+}
+
+void solve() {
+	int n; cin >> n;
+	vi v;
+	for (int i = 1; i <= n; i++)
+		v.push_back(i);
+	cout << n << endl;
+	int t = n;
+	for (int i = 0; i < n; i++) {
+		print(v);
+		if (t >= 2)
+			swap(v[t - 1], v[t - 2]);
+		t--;
+	}
+}
+
+int main() {
+	fast;
+	int t; cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}

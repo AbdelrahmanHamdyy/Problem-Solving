fd9de478fc23b7065b65843babd4f1efabfe298d
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-03-01 00:16:01 +0200

    Update Soldier and Bananas.cpp

--- CodeForces/Old Contests Problems/Soldier and Bananas.cpp
+++ CodeForces/Old Contests Problems/Soldier and Bananas.cpp
@@ -5,14 +5,17 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 #define sz(v) v.size()
 #define cin(v) for (int i = 0; i < sz(v); i++) cin >> v[i];
-#define cout(v) for (auto i : v) cout << i << endl;
-constexpr int pi = 3.14159265358979323846;
+#define cout(v) for (auto i : v) cout << i << " ";
+constexpr double pi = 3.14159265358979323846;
 
 typedef long long ll;
+typedef unsigned long long usll;
 typedef vector<int> vi;
 typedef vector<ll> vl;
 typedef vector<char> vc;
 typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef set<int> si;
 
 ll gcd(ll a, ll b) {
 	if (b == 0)
@@ -22,16 +25,66 @@ ll gcd(ll a, ll b) {
 ll lcm(ll a, ll b) {
 	return a * b / gcd(a, b);
 }
+ll fact(ll n) {
+	return n == 1 ? 1 : n * fact(n - 1);
+}
+
+ll nCr(ll n, ll r) {
+	ll g, u = 1, d = 1;
+	for (ll i = 0; i < r; i++) {
+		u = u * (n - i);
+		d = d * (r - i);
+		g = gcd(u, d);
+		u /= g;
+		d /= g;
+	}
+	return u;
+}
+
+bool isPrime(int n) {
+	for (int i = 2; i < n; i++)
+		if (n % i == 0) return false;
+	return true;
+}
+
+int getFirstDivisor(int n) {
+	for (int i = 2; i < n; i++)
+		if (n % i == 0) return i;
+}
+
+int getSumOfDigits(int n) {
+	int ans = 0;
+	while (n) {
+		ans += n % 10, n /= 10;
+	}
+	return ans;
+}
+
+void print(vl& v) {
+	int len = sz(v);
+	for (int i = 0; i < len; i++) {
+		if (i < len - 1) cout << v[i] << " ";
+		else cout << v[i];
+	}
+	cout << endl;
+}
 
 void solve() {
 	ll k, n, w; cin >> k >> n >> w;
-	ll total = (w / 2.0) * (1 + w);
-	total *= k;
-	ll b = total - n;
-	if (b < 0)
-		cout << 0;
-	else
-		cout << b;
+	ll amountBorrowed = 0;
+	bool flag = false;
+	for (ll i = 1; i <= w; i++) {
+		if (flag) {
+			amountBorrowed += (i * k);
+		}
+		else if ((n - (i * k)) < 0 && !flag) {
+			amountBorrowed += ((i * k) - n);
+			n = 0;
+			flag = true;
+		}
+		else n -= (i * k);
+	}
+	cout << amountBorrowed;
 }
 
 int main() {

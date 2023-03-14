0e502d8ab74c915da72720657c0b9397730fe9f1
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-03-14 22:03:39 +0200

    #853 Div2 B

---
+++ CodeForces/2023/Round #853 (Div 2)/Serval and Inversion Magic.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+
+#define fast ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define all(v) v.begin(), v.end()
+#define sz(v) v.size()
+#define cin(v) for (int i = 0; i < sz(v); i++) cin >> v[i];
+#define cout(v) for (auto i : v) cout << i << " ";
+constexpr double pi = 3.14159265358979323846;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<bool> vb;
+typedef set<int> si;
+
+vb isVisited(10000001, false);
+
+void dfs(vector<vi> &graph, int node) {
+	isVisited[node] = true;
+	for (auto child : graph[node]) {
+		if (!isVisited[child])
+			dfs(graph, child);
+	}
+}
+
+void bfs(vector<vi> graph, int node) {
+	queue<int> nextToVisit;
+	nextToVisit.push(node);
+	while (!nextToVisit.empty()) {
+		int current = nextToVisit.front();
+		isVisited[current] = true;
+		nextToVisit.pop();
+		for (auto child : graph[current]) {
+			if (!isVisited[child]) {
+				isVisited[child] = true;
+				nextToVisit.push(child);
+			}
+		}
+	}
+}
+
+vl prime_factors(ll n) {
+	vl primes;
+	for (ll i = 2; i * i <= n; i++) {
+		while (n % i == 0) {
+			primes.push_back(i);
+			n /= i;
+		}
+	}
+	if (n > 1)
+		primes.push_back(n);
+	return primes;
+}
+
+ll gcd(ll a, ll b) {
+	if (b == 0)
+		return a;
+	return gcd(b, a % b);
+}
+
+ll lcm(ll a, ll b) {
+	return a * b / gcd(a, b);
+}
+
+ll fact(ll n) {
+	return n == 1 ? 1 : n * fact(n - 1);
+}
+
+ll nCr(ll n, ll r) {
+	ll g, u = 1, d = 1;
+	for (ll i = 0; i < r; i++) {
+		u = u * (n - i);
+		d = d * (r - i);
+		g = gcd(u, d);
+		u /= g;
+		d /= g;
+	}
+	return u;
+}
+
+bool isPrime(int n) {
+	for (int i = 2; i < n; i++)
+		if (n % i == 0) return false;
+	return true;
+}
+
+int getFirstDivisor(int n) {
+	for (int i = 2; i < n; i++)
+		if (n % i == 0) return i;
+}
+
+int getSumOfDigits(int n) {
+	int ans = 0;
+	while (n) {
+		ans += n % 10, n /= 10;
+	}
+	return ans;
+}
+
+ll countDigits(ll n) {
+	ll ans = 0;
+	while (n) {
+		ans++;
+		n /= 10;
+	}
+	return ans;
+}
+
+void print(vl& v) {
+	ll len = sz(v);
+	for (ll i = 0; i < len; i++) {
+		if (i < len - 1) cout << v[i] << " ";
+		else cout << v[i];
+	}
+	cout << endl;
+}
+
+void solve() {
+	ll n, c = 0; cin >> n;
+	string s; cin >> s;
+	if (s[0] != s[n - 1]) c++;
+	for (ll i = 1; i < n / 2; i++) {
+		if (s[i - 1] == s[n - i] && s[i] != s[n - i - 1]) c++;
+		if (c > 1) break;
+	}
+	c > 1 ? cout << "NO\n" : cout << "YES\n";
+}
+
+int main() {
+	fast;
+	int t; cin >> t;
+	while (t--)
+		solve();
+	return 0;
+}

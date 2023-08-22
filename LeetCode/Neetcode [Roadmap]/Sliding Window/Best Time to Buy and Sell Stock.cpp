6cf288145ce2803e9f1b318dce46b192e6fcc24c
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2023-08-22 03:14:53 +0300

    Update Sliding Window [1]

--- LeetCode/Neetcode [Roadmap]/Sliding Window/Best Time to Buy and Sell Stock.cpp
+++ LeetCode/Neetcode [Roadmap]/Sliding Window/Best Time to Buy and Sell Stock.cpp
@@ -1,20 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        // vector<pair<int, int>> vp;
-        // int n = prices.size();
-        // for (int i = 0; i < n; i++) vp.push_back({prices[i], i});
-        // sort(vp.begin(), vp.end());
-        // int i = 0, j = n - 1;
-        // while (i < j) {
-        //     if (vp[j].second > vp[i].second) return vp[j].first - vp[i].first;
-        //     else if (vp[j].first - vp[j - 1].first < vp[i + 1].first - vp[i].first) j--;
-        //     else i++;
-        // }
-        // return 0;
-
         ///////////////////// SLIDING WINDOW /////////////////////
-
         int n = prices.size();
         int l = INT_MAX, p = 0, ans = 0;
         for (int i = 0; i < n; i++) {
